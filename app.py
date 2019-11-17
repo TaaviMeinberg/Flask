b0aee9579405f76701ace3690f40eb8a0cae2cb9
TaaviMeinberg <taavi.meinberg@gmail.com>
2019-11-17 12:55:40 +0200

    Pokemon'i info kuvamine koos pildiga

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def index():
 @app.route('/pokemon', methods=['GET', 'POST'])
 def get_pokemon():
     if request.method == 'POST':
-        pokemonName = request.form.get('pokemon_name')
+        pokemonName = request.form.get('pokemonName')
         pokemonData = requests.get(url = 'https://pokeapi.co/api/v2/pokemon/'+pokemonName)
         data = pokemonData.json()
-        print(data['name'])
-        return render_template('pokemon.html', name = data['name'])
-    return render_template('pokemon.html', name = '')
+        print()
+        return render_template('pokemon.html', ID = data['id'], name = data['name'], image = data['sprites']['front_default'])
+    return render_template('pokemon.html', ID = '', name = '', image = '')
 
 if __name__ == '__main__':
     app.run()
